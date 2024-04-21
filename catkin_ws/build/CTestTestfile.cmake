12e5a880b36ed98ec8fe18f38acb24af1fe49dc9
Haocheng <haocheng382@gmail.com>
2024-04-21 03:02:00 -0700

    change structure

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/x57/ROS_TIAGo_ws/catkin_ws/src
+# Build directory: /home/x57/ROS_TIAGo_ws/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
