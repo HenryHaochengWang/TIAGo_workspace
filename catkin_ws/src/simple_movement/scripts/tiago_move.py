dbb529d5882e5a52e1fb6d0efe60baa44309d617
Haocheng Wang <Haocheng382@gmail.com>
2024-06-04 20:59:11 -0700

    simple movement

---
+++ catkin_ws/src/simple_movement/scripts/tiago_move.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import rospy
+from geometry_msgs.msg import Twist
+
+if __name__ == '__main__':
+    rospy.init_node("run_circle")
+    rospy.loginfo("Node has been started")
+
+    pub = rospy.Publisher("/mobile_base_controller/cmd_vel", Twist, queue_size=10)
+
+    rate = rospy.Rate(2)
+
+    # limit running time in 10 seconds
+    start_time = rospy.Time.now()
+
+    try:
+        while not rospy.is_shutdown() and rospy.Time.now() - start_time < rospy.Duration(50):
+            rospy.loginfo("Publishing cmd_vel message")
+            # publish cmd_vel message
+            msg = Twist()
+            msg.linear.x = 2.0
+            msg.angular.z = -3.0
+            pub.publish(msg)
+            rate.sleep()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        rospy.loginfo("Stopping the robot")
+        msg = Twist()
+        msg.linear.x = 0.0
+        msg.angular.z = 0.0
+        pub.publish(msg)
+        rospy.loginfo("Node has been stopped")
